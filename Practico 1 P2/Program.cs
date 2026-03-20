6ed7659396f62fd65507ca9387b4fb3970379d40
bixis <julianlinarescalero@gmail.com>
2026-03-20 09:49:10 -0300

    ejercicio 2 + TODO.md

--- Practico 1 P2/Program.cs
+++ Practico 1 P2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System; // Tuve que agregar esta linea porque el código tenia muchos errores, no la vimos en clase pero la busqué en google.
+using System.Runtime.CompilerServices;
 
 namespace Practico_1_P2;
 
@@ -17,6 +18,7 @@ class Program
             Console.WriteLine("Qué ejercicio desea ver?");
             Console.WriteLine("");
             Console.WriteLine("1- Ejercicio 1 - 'Adivine el número random'");
+            Console.WriteLine("2- Ejercicio 2 - 'Ver tabla de multiplicación'");
             Console.WriteLine("0- Salir");
 
             string opcion = Console.ReadLine();
@@ -25,38 +27,46 @@ class Program
                 case "1":
                     EjercicioUno();
                     break;
+                case "2":
+                    EjercicioDos();
+                    break;
                 case "0":
                     salir = true;
                     break;
             }
         }
     }
-    
-    
+
+
     // Ejercicio 1.
     // Solicitar números hasta que ingrese el 0 (fin del ingreso).
     // Se debe comparar con un número random e indicar cuando son iguales.
     static void EjercicioUno()
     {
         Random randomObjeto = new Random();
-        int randomEntero = randomObjeto.Next(1,11); // Excluyo 0 porque lo utilizamos para salir del programa, del 1 al 10 para simplificar.
+        int randomEntero = randomObjeto.Next(1, 11); // Excluyo 0 porque lo utilizamos para salir del programa, del 1 al 10 para simplificar.
         string random = randomEntero.ToString();
-        bool salirDelEjericio = false;
-        
+        bool salirDelEjercicio = false;
+
+
+        Console.WriteLine("");
         Console.WriteLine("Ingrese un número hasta que coincida con el número random.");
         Console.WriteLine("Ingrese 0 para salir del ejercicio");
-        
-        while (!salirDelEjericio)
+        Console.WriteLine("");
+
+        while (!salirDelEjercicio)
         {
             string numeroIngresado = Console.ReadLine();
 
             if (numeroIngresado == random)
             {
-                Console.WriteLine($"Usted adivinó el número {random} correctamente!");   
+                Console.WriteLine($"Usted adivinó el número {random} correctamente!");
+                Console.WriteLine("");
+                break;
             }
             else if (numeroIngresado == "0")
             {
-                salirDelEjericio = true;
+                salirDelEjercicio = true;
             }
             else
             {
@@ -65,4 +75,65 @@ class Program
         }
 
     }
+
+
+    // Ejercicio 2.
+    // Solicitar números hasta que ingrese el 0 (fin del ingreso).
+    // Se debe mostrar la tabla del mismo.
+    static void EjercicioDos()
+    {
+        bool salirDelEjercicio = false;
+
+
+        Console.WriteLine(" ");
+        Console.WriteLine("Ingrese un número para ver su tabla de multiplicación.");
+        Console.WriteLine("Ingrese 0 para salir del ejercicio.");
+        Console.WriteLine(" ");
+
+        while (!salirDelEjercicio)
+        {
+            string numeroIngresado = Console.ReadLine();
+            int numeroIngresadoEntero = 0;
+            bool ok = int.TryParse(numeroIngresado, out numeroIngresadoEntero);
+
+            if (!ok)
+            {
+
+                Console.WriteLine("Usted debe dar un string!");
+                Console.WriteLine(" ");
+                break;
+            }
+            else if (numeroIngresadoEntero == 0)
+            {
+                salirDelEjercicio = true;
+                break;
+            }
+            else if (numeroIngresadoEntero != 0)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine($"Tabla del {numeroIngresado}");
+                Console.WriteLine(" ");
+                for (int i = 1; i < 10; i++) // tampoco lo vimos en clase pero lo busqué para simplificar.
+                {
+                    Console.WriteLine($"{numeroIngresadoEntero} x {i} = {(numeroIngresadoEntero * i)}");
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                break;
+            }
+        }
+
+    }
+
+
+    // Ejercicio 3
+    // Solicitar dos números (controlar que el primer número sea menor al segundo) y muestre todos los números entre los valores ingresados que sean pares. Para salir se debe ingresar 0.
+    static void EjercicioTres()
+    {
+	    // logica
+    }
+    
+
+
+
 }
---
+++ Practico 1 P2/TODO.md
@@ -0,0 +1,22 @@
+# TODO
+
+## Features
+- [x] Ejercicio 1
+- [x] Ejercicio 2
+- [] Ejercicio 3
+- [] Ejercicio 4
+- [] Ejercicio 5
+- [] Ejercicio 6
+- [] Ejercicio 7
+- [] Ejercicio 8
+- [x] Ejercicio 9 (Menu hecho en clase, ya integrado en la solucion)
+
+## Bugs
+- [x] El ejercicio 2 nos da la tabla del 0 cuando ingresamos un string vacio
+- [x] El ejercicio 1 tiene un problema, cuando adivino el numero sigo jugando. lo ideal seria avisar que ganaste, salir del loop y volver al menu principal
+- []
+- []
+
+## Ideas
+- [] Agregar un clear console cada vez que cambio de Menu
+- [x] Poner todos los ejercicios dentro de un menu centralizado
